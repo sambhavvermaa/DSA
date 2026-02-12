94a2999c3f5f3cb1a80db020029cf5b6f0e2d575
sambhavvermaa <2300301530104@ipec.org.in>
2026-02-12 09:46:58 +0530

    Add linear search implementation

---
+++ linearsearch.java
@@ -0,0 +1,20 @@
+public class linearsearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int target = 5;
+        int ans = linearsearch(arr,target);
+        System.out.println(ans);
+    }
+    static int linearsearch(int[] arr,int target){
+        if (arr.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+}
